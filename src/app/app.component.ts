4850b229ac994fa77b01afbb95ace110bb327cc7
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-24 23:53:18 +0200

    added above the fold component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AboveTheFoldComponent } from './main-content/above-the-fold/above-the-fold.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
