2574e085422fcb3c0903d880e71d5c3b2538455a
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-25 09:04:38 +0200

    added abot-me component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AboveTheFoldComponent } from './main-content/above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent],
+  imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
