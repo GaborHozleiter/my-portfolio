44187511856722b5ee68a96da8ba8e737bf25f97
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-25 21:16:24 +0200

    addde icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AboveTheFoldComponent } from './main-content/above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './main-content/about-me/about-me.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent, AboutMeComponent],
+  imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent, AboutMeComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
