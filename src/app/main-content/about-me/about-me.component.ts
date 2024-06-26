e2eeb317edc373d88af5e7972069fa871768c2e9
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-26 05:31:04 +0200

    addded image and background

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
 
+    hoveredBulb: boolean = false;
+    hoveredPuzzle:boolean = false;
+
+    changeBulbImage(){
+      this.hoveredBulb = true;
+    }
+
+    changePuzzleImage(){
+      this.hoveredPuzzle = true;
+    }
 }
