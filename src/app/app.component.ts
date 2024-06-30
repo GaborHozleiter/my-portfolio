f549eead8ec4ef7624c8be2e57e70c58d36f0061
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-30 16:54:09 +0200

    constructed and designed contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { AboveTheFoldComponent } from './main-content/above-the-fold/above-the-f
 import { AboutMeComponent } from './main-content/about-me/about-me.component';
 import { SkillsComponent } from './main-content/skills/skills.component';
 import { PortfolioComponent } from './main-content/portfolio/portfolio.component';
+import { ContactComponent } from './main-content/contact/contact.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
+  imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent, AboutMeComponent, SkillsComponent, PortfolioComponent,
+    ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
