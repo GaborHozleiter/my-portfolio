ef929b5d0889ec1a36ad15cee8fe22246dc2f381
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-01 18:28:49 +0200

    added active button style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { AboutMeComponent } from './main-content/about-me/about-me.component';
 import { SkillsComponent } from './main-content/skills/skills.component';
 import { PortfolioComponent } from './main-content/portfolio/portfolio.component';
 import { ContactComponent } from './main-content/contact/contact.component';
+import { FooterComponent } from './shared/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, AboveTheFoldComponent, AboutMeComponent, SkillsComponent, PortfolioComponent,
-    ContactComponent],
+    ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
