84ab9b203a036b4a2a47e6439e72800d54096111
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-02 11:28:23 +0200

    optimize project-ordner structur

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../above-the-fold/above-the-fold.component';
-import { AboutMeComponent } from '../about-me/about-me.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { ContactComponent } from '../contact/contact.component';
