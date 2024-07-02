721866c4ac9485d9bbdc7b6c6b280b3ebdbb885b
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-03 01:45:17 +0200

    added translate

--- src/app/translate.service.ts
+++ src/app/translate.service.ts
@@ -13,12 +13,15 @@ export class TranslateService {
   iAmGerman : string = 'Ich bin';
   letsTalkEnglish : string = 'Let’s talk!';
   letsTalkGerman : string = 'Lass uns reden!';
-  aboutMeTextTopEnglisch : string = 'Hello. My name is Gabor and I am from Seelze in Lower Saxony. I work on various programming projects with great enthusiasm and welcome every new challenge.';
-  aboutMeTextCenterEnglisch : string = 'I have extensive knowledge in front-end development and am always open to new technologies. My ability to quickly adapt to new tools and trends allows me to develop innovative solutions.';
-  aboutMeTextBottomEnglisch : string = 'Through the Developer Academy, I have expanded my frontend development skills and am confident and competent in software development. I look forward to applying and deepening my knowledge in a new job.';
+  aboutMeTextTopEnglish : string = 'Hello. My name is Gabor and I am from Seelze in Lower Saxony. I work on various programming projects with great enthusiasm and welcome every new challenge.';
+  aboutMeTextCenterEnglish : string = 'I have extensive knowledge in front-end development and am always open to new technologies. My ability to quickly adapt to new tools and trends allows me to develop innovative solutions.';
+  aboutMeTextBottomEnglish : string = 'Through the Developer Academy, I have expanded my frontend development skills and am confident and competent in software development. I look forward to applying and deepening my knowledge in a new job.';
   aboutMeTextTopGerman : string = 'Hallo. Ich heiße Gabor und komme aus Seelze in Niedersachsen. Mit großer Begeisterung arbeite ich an verschiedenen Programmierprojekten und freue mich über jede neue Herausforderung.';
   aboutMeTextCenterGerman : string = 'Ich habe umfassende Kenntnisse in der Frontend-Entwicklung und bin stets offen für neue Technologien. Meine Fähigkeit, mich schnell in neue Tools und Trends einzuarbeiten, ermöglicht es mir, innovative Lösungen zu entwickeln.';
   aboutMeTextBottomGerman : string = 'Durch die Developer Akademie habe ich meine Frontend-Entwicklungsfähigkeiten ausgebaut und bin kompetent in der Software-Entwicklung. Ich freue mich darauf, mein Wissen in einem neuen Job einzusetzen und zu vertiefen.';
+  skillsDescriptionEnglish : string = 'I have gained experience in building projects with various frontend technologies and concepts.';
+  skillsDescriptionGerman : string = 'Ich habe Erfahrung im Erstellen von Projekten mit verschiedenen Frontend-Technologien und -Konzepten gesammelt.';
+  skillsButtonText : string = 'Get in touch';
 
   constructor() { }
 }
