fd5b6dcf345e64081db6e89cf3a641ce03d8adf4
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-03 18:54:01 +0200

    added new email adress

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'DEINE@MAIL.de';  
+            $recipient = 'developer@gabor-hozleiter.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
