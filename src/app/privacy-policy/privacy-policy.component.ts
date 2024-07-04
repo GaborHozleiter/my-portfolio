8b6c429431d69585aba50675f77f81a221eabf66
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-04 12:45:24 +0200

    added links and changed email-adress

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
 })
 export class PrivacyPolicyComponent {
 
-  emailAdress : string = 'hozleitergabor@gmail.com';
+  emailAdress : string = 'developer@gabor-hozleiter.com';
 }
