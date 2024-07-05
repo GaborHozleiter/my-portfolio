2091abad0fb03b2d5ec86125d91d2e38437bf5e6
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-05 13:00:25 +0200

    added live test links

--- src/app/main-content/portfolio-project/portfolio-project.component.ts
+++ src/app/main-content/portfolio-project/portfolio-project.component.ts
@@ -19,7 +19,7 @@ export class PortfolioProjectComponent {
       skills : 'JavaScript | HTML | CSS | Firebase',
       descriptionEnglish : 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       descriptionGerman : 'Task-Manager inspiriert vom Kanban-System. Aufgaben erstellen und organisieren per Drag-and-Drop, Nutzer und Kategorien zuweisen.',
-      testLink : '#',
+      testLink : 'https://join.gabor-hozleiter.com/',
       githubLink : 'https://github.com/GaborHozleiter/Join',
       background : '../../../assets/img/Property 1=join photo.png',
       backgroundNotebook : '../../../assets/img/Join.png'
@@ -29,7 +29,7 @@ export class PortfolioProjectComponent {
       skills : 'JavaScript | HTML | CSS',
       descriptionEnglish : 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
       descriptionGerman : 'Jump and Run Spiel auf objektorientierter Basis. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.',
-      testLink : '#',
+      testLink : 'https://pollo-loco.gabor-hozleiter.com/',
       githubLink : 'https://github.com/GaborHozleiter/El_Pollo_Loco',
       background : '../../../assets/img/pollo loco background.png',
       backgroundNotebook : '../../../assets/img/Pollo loco.png'
