4a4e1fec7e139b36052ca7fdd77da35fe529dcfd
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-09-15 01:05:28 +0200

    deleted old portfolio-project component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboveTheFoldComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, MyPortfolioComponent,
+  imports: [AboveTheFoldComponent, AboutMeComponent, SkillsComponent, MyPortfolioComponent,
     ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
